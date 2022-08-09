d3b5ff6b692674c6edf672bd620484abfc23dbb4
Andrew Moore <avmoore@umass.edu>
2022-08-08 22:14:48 -0400

    Expanded item list and started building the table

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Create the user login interface 
 let userLoginInterface = document.getElementById("userLogin");
 
 let div = document.createElement('div');
@@ -6,11 +7,74 @@ div.innerText="Test";
 
 userLoginInterface.appendChild(div);
 
-let graphic = document.getElementById("inventoryContainer");
+// Create the container for the inventory
 
-graphic.innerHTML = '';
+let container = document.getElementById("inventoryContainer");
+container.innerHTML = '';
 
+
+// Show the current item image, details
+let currentItem = document.createElement('div');
+currentItem.classList.add("description");
 let img = document.createElement('img');
 img.src = './test.jpg';
 
-graphic.appendChild(img);
+currentItem.appendChild(img);
+
+let desc = document.createElement('p')
+desc.classList.add('description');
+desc.innerText = "This is a test description";
+
+currentItem.appendChild(desc);
+
+// Create a scrollable table containing inventory items
+const testItems =
+[
+    "Sword+1",
+    "Staff",
+    "Mage Hand Scroll",
+    "Axe+3",
+    "Staff of Rage",
+    "Fire Arrows",
+    "Deaths Embrace",
+    "Ice Arrows",
+    "Poison Dagger"
+];
+
+let inventoryList = document.getElementById("inventoryList");
+let inventoryTable = document.createElement('table');
+inventoryTable.classList.add("inventoryTable");
+
+// Add the headers for the items
+let itemName = document.createElement('th');
+itemName.classList.add("inventoryTable");
+itemName.innerText = "Weapon";
+let quantity = document.createElement('th');
+quantity.classList.add("inventoryTable");
+quantity.innerText = "Amount";
+
+inventoryTable.appendChild(itemName);
+inventoryTable.appendChild(quantity);
+
+let tableBody = document.createElement('tbody');
+
+for (let item in testItems)
+{
+    let row = document.createElement('tr');
+    row.classList.add("inventoryTable");
+    let invI = document.createElement('td');
+    invI.classList.add("inventoryTable");
+
+    invI.innerText = testItems[item];
+
+    row.appendChild(invI);
+    tableBody.appendChild(row);
+
+}
+
+inventoryTable.appendChild(tableBody);
+
+// Append all items to the container to display them
+container.appendChild(currentItem);
+inventoryList.appendChild(inventoryTable);
+
